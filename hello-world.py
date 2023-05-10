7944ad186fbd3a96ef38ec2342f0575870e15cb7
Saurabh Tripathi <saurav9048@gmail.com>
2023-05-10 13:00:42 +0530

    my first git commit

---
+++ hello-world.py
@@ -0,0 +1,11 @@
+# Print something
+print('Hello world!')
+
+# Perform a calculation
+x = 2 + 3
+print('The value of x is {}'.format(x))
+
+# Import a module
+import math
+y = 2.3
+print('The number {} can be rounded up to {}'.format(y, math.ceil(y)))
